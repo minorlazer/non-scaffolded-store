1dad8b63f4d4c9a66303eabad294258a56c34cfd
Carlo Duaqui <carloduaqui@outlook.com>
2015-03-17 10:24:41 -0500

    Implemented the show, new create controls

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,30 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
   # The associated view app/views/products/index.html.erb is auto-loaded
+
+  def show
+    @product = Product.find(params[:id])
+  end
+  # The associated view app/views/products/show.html.erb is auto-loaded
+
+  def new
+    @product = Product.new
+  end
+  # auto loads app/views/products/new.html.erb
+
+  def create
+    @product = Product.new(whitelisted_params)
+    if (@product.save)
+      redirect_to @product # redirect to the show action for this product
+    else
+      render text: @product.errors.inspect
+    end
+  end
+
   # The view gets access to all the action's instance variables
+
+  private
+  def whitelisted_params
+    params.require(:product).permit(:name, :description, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # Create
+  get  'products/new' => 'products#new' # Show a new product form
+  post 'products'     => 'products#create' #create the new product
+
+  # Read
   get 'products', to: 'products#index'
   #get 'products' => 'products#index'
+  get 'products/:id' => 'products#show', as: 'product' #show a member of products (id)
+
+
+  # Update
+  # Destroy
 end
