0ceb140f4664bea9b79b37ee659b737c05eb68dc
Carlo Duaqui <carloduaqui@outlook.com>
2015-03-20 11:12:34 -0500

    Implemented the rest of the CRUD task on Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :load_product_by_id, only: [:show, :update, :edit, :destroy]
   def index
     # Use the Product model to load all products
     @products = Product.all
@@ -6,7 +7,6 @@ class ProductsController < ApplicationController
   # The associated view app/views/products/index.html.erb is auto-loaded
 
   def show
-    @product = Product.find(params[:id])
   end
   # The associated view app/views/products/show.html.erb is auto-loaded
 
@@ -20,14 +20,38 @@ class ProductsController < ApplicationController
     if (@product.save)
       redirect_to @product # redirect to the show action for this product
     else
-      render text: @product.errors.inspect
+      render :new
     end
   end
+  # no associated vew. no app/views/products/create.html.erb
+  # if products saves, redirect to the show
+  # if the validations fail we render the view associated with the new action.
 
+  def edit
+  end
+
+  def update
+    if (@product.update_attributes(whitelisted_params))
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+  # There is no app/views/products/update.html.erb
+  # We either redirect on success or rende rthe edit view on failure
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
   # The view gets access to all the action's instance variables
 
   private
   def whitelisted_params
     params.require(:product).permit(:name, :description, :price)
+  end
+
+  def load_product_by_id
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
+  # HTTP Verbs
+  # Get
+  # Post
+  # Patch
+  # Delete
+
+  root to: 'products#index'
+
   # Create
-  get  'products/new' => 'products#new' # Show a new product form
+  get  'products/new' => 'products#new', as: 'new_product' # Show a new product form
   post 'products'     => 'products#create' #create the new product
 
   # Read
-  get 'products', to: 'products#index'
-  #get 'products' => 'products#index'
-  get 'products/:id' => 'products#show', as: 'product' #show a member of products (id)
-
+  get 'products', to: 'products#index' #get 'products' => 'products#index'
+  get 'products/:id' => 'products#show', as: 'product', id: /\d+/ #show a member of products (id)
 
   # Update
+  get 'products/:id/edit' => 'products#edit', as: 'edit_product', id: /\d+/
+  patch 'products/:id' => 'products#update', id: /\d+/
+
   # Destroy
+  delete 'products/:id' => 'products#destroy', as: 'destroy_product', id: /\d+/
 end
